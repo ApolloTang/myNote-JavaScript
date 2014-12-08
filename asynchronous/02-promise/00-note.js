e7c773d0a46eb40296100d049038ed2bc4ca2f16
Apollo Tang <tang.apollo@gmail.com>
2014-12-08 13:36:47 -0500

    add indents to note for readibility

--- asynchronous/02-promise/00-note.js
+++ asynchronous/02-promise/00-note.js
@@ -18,25 +18,29 @@
 
     var promise1 = $('#label-1').animate({ opacity: 0.25 }, 100).promise();
     var promise2 = $('#label-2').animate({ opacity: 0.75 }, 200).promise();
-    $.when(promise1, promise2).done(function(){
+    $.when(
+          promise1
+        , promise2
+    ).done(function(){
             // run these codes when both animations are done.
     });
 
 // more example
 
-    $.when($.ajax('http://google.com'), $.ajax('http://yahoo.com')).then(
-        function(googlePage, yahooPage){
+    $.when(
+          $.ajax('http://google.com')
+        , $.ajax('http://yahoo.com')
+    ).then( function(googlePage, yahooPage){
             // Both URLs have been fetched.
-        }
-    );
+    });
 
 
     $.when(
           $.ajax('http://google.com').then(function(){ return $('#label_1').animate({ opacity: 0.25 }, 100); })
         , $.ajax('http://yahoo.com').then(function(){ return $('#label_2').animate({ opacity: 0.75 }, 200); })
-    ).then(
-        function(){/* Both URLs have been fetched and both animations have completed.*/ }
-    );
+    ).then( function(){
+        /* Both URLs have been fetched and both animations have completed.*/
+    });
 
 
 // .... hmmmm.... what is the different between "done" and "then"  ?
