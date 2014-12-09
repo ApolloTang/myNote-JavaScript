1833310dd4d9fc9efb4dc0404e5cbee54e03a609
Apollo Tang <tang.apollo@gmail.com>
2014-12-09 12:38:06 -0500

    asynchronous:: edit $.when.js

--- asynchronous/02-promise/$.when.js
+++ asynchronous/02-promise/$.when.js
@@ -27,3 +27,26 @@
        calls to callAfter will need to be found and changed.
     3. What happens if firstAction or secondAction doesn’t return a promise?
     4. The return value of finalize is lost.
+
+
+    The jQuery $.when function (with some help from then) takes care
+    of all these issues, allowing you to simply write:
+
+        $.when( firstAction() , secondAction()).then(finalize);
+
+    The call to $.when returns a new promise, as does the call to
+    then. If either firstAc tion or secondAction rejects the promise
+    it returns, the error value will be propagated to finalize.
+
+    When it becomes available, it will be used to resolve the promise
+    returned by then(). So if you need the result, you can simply write:
+
+        $.when(firstAction(), secondAction()).then(finalize).done(function(){
+            // arguments holds the result returned by the finalize function.
+        });
+
+    $.when also correctly handles the case in which the value returned
+    by either (or both) of firstAction or secondAction is not a deferred.
+
+
+
