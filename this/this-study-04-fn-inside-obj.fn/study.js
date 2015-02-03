55cee3b802deab2a30384ae5c0d78936700568dd
Apollo Tang <tang.apollo@gmail.com>
2015-02-03 17:13:02 -0500

    this: added comments

--- this/this-study-04-fn-inside-obj.fn/study.js
+++ this/this-study-04-fn-inside-obj.fn/study.js
@@ -2,10 +2,10 @@
 var o = {
     fn1: function() {
         var that = this;
-        console.log('1 ' +  (o === this) );
+        console.log('1 ' +  (o === this) ); // true
         function fn2() {
-            console.log ('2 ' + (o===this));
-            console.log ('3 ' + (o===that));
+            console.log ('2 ' + (o===this)); // false
+            console.log ('3 ' + (o===that)); // true
         }
 
         fn2();
